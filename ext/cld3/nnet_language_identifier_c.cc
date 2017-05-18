f3ba1fd91880f482fdfc8b9c3892921ffff6a5df
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-05-18 10:23:12 +0900

    ext: Remove redundant cstddef

--- ext/cld3/nnet_language_identifier_c.cc
+++ ext/cld3/nnet_language_identifier_c.cc
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <cstddef>
 #include <iostream>
 #include "nnet_language_identifier.h"
 
