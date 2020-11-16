0c44c7d8ab68aa388b7e22292beeb0b85f4b796b
Akihiko Odaki <nekomanma@pixiv.co.jp>
2020-11-16 22:32:37 +0900

    Update email address to nekomanma@pixiv.co.jp

--- ext/cld3/extconf.rb
+++ ext/cld3/extconf.rb
@@ -1,4 +1,4 @@
-# Copyright 2017 Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
+# Copyright 2017 Akihiko Odaki <nekomanma@pixiv.co.jp>
 # All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
